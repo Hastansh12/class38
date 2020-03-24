7604ff22828a0d5f453ae9f18b7370b6c3a9b70e
Hastansh12 <hastu4123@gmail.com>
2020-03-24 16:53:58 +0530

    Images added

--- js/Game.js
+++ js/Game.js
@@ -30,10 +30,15 @@ class Game {
     }
 
     car1 = createSprite(100,200);
+    car1.addImage('car1',car1Image);
     car2 = createSprite(300,200);
+    car2.addImage('car2',car2Image);
     car3 = createSprite(500,200);
+    car3.addImage('car3',car3Image);
     car4 = createSprite(700,200);
+    car4.addImage('car4',car4Image);
     cars = [car1, car2, car3, car4];
+
   }
 
   play(){
@@ -42,13 +47,15 @@ class Game {
     Player.getPlayerInfo();
     
     if(allPlayers !== undefined){
-      //var display_position = 100;
       
+      //var display_position = 100;
+    background('black');
+     image(trackImage,0,-displayHeight*4,displayWidth,displayHeight*5);
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 200;
       var y;
 
       for(var plr in allPlayers){
@@ -67,7 +74,7 @@ class Game {
           camera.position.x = displayWidth/2;
           camera.position.y = cars[index-1].y
         }
-       
+        console.log(allPlayers[plr].distance)
         //textSize(15);
         //text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
       }
@@ -77,6 +84,10 @@ class Game {
     if(keyIsDown(UP_ARROW) && player.index !== null){
       player.distance +=10
       player.update();
+    }
+
+    if(player.distance>=4100){
+      gameState=2;
     }
 
     drawSprites();
